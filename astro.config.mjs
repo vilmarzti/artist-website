60c12d88bffcd2ea3eaa4374ca7705407a18bc17
shine <villavicencio.martin@pm.me>
2026-09-04 13:14:20 +0200

    fixed typo in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vilmarzti.github.io"
+    site: "https://vilmarzti.github.io",
     base: "/artist-website"
 });
