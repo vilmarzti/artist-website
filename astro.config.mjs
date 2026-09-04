f1b4a89f8fef1b80e2e87595329d6377fc0304c6
shine <villavicencio.martin@pm.me>
2026-09-04 13:13:41 +0200

    dev branch site-url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vilmarzti.github.io"
-    base: "/artist-website"
+    site: "https://localhost"
 });
