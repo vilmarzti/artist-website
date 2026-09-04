b74ca360f4e6a1389d83c5fc3e39f5b495a78411
shine <villavicencio.martin@pm.me>
2026-09-04 18:18:16 +0200

    trailing Slashes

--- astro.config.prod.mjs
+++ astro.config.prod.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://vilmarzti.github.io",
-    base: "/artist-website"
+    base: "/artist-website",
+    trailingSlash: "always"
 });
